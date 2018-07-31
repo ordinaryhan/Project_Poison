a5e0fe5139e2a3e4867411859422d5a10c875305
GibbemLee <rlqma1226@gmail.com>
2018-07-31 15:13:29 +0900

    시작 애니메이션 구현
    시작 애니메이션 및 수집창 변수 설정

--- 시작화면/Project Poison/Assets/0_main/script/s_collection.cs
+++ 시작화면/Project Poison/Assets/0_main/script/s_collection.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class s_collection : MonoBehaviour {
     public enum Collect { one, two, three, four }
     public Collect collectNum;
+
+    private int[] score = new int[4];
+    private bool[] grade = new bool[4];
     private bool[] iscollect = new bool[4];
+
+    public GameObject Collection;
     public GameObject rock;
     public GameObject image;
-
 
     void Start()
     {
@@ -16,14 +20,47 @@ public class s_collection : MonoBehaviour {
         image.SetActive(false);
         for (int i = 0; i < 4; i++)
         {
+            score[i] = 0;
+            grade[i] = false;
             iscollect[i] = false;
         }
     }
 
     void Update() {
+        Score();
         Result();
     }
+    public void Score()
+    {
+        if (score[0] > 420) grade[0] = true;
+        if (score[0] <= 420) grade[0] = false;
+        if (score[1] > 250) grade[1] = true;
+        if (score[1] <= 250) grade[1] = false;
+        if (score[2] > 150) grade[2] = true;
+        if (score[2] <= 150) grade[2] = false;
 
+        if (collectNum == Collect.one)
+        {
+            if ((grade[0] && grade[1] && grade[2]) || (grade[0] && grade[1] && !grade[2]))
+                iscollect[0] = true;
+        }
+        if(collectNum == Collect.two)
+        {
+            if ((grade[0] && !grade[1] && grade[2]) || (grade[0] && !grade[1] && !grade[2]))
+                iscollect[1] = true;
+        }
+        if(collectNum == Collect.three)
+        {
+            if ((!grade[0] && grade[1] && grade[2]) || (!grade[0] && grade[1] && !grade[2]))
+                iscollect[2] = true;
+        }
+        if(collectNum == Collect.four)
+        {
+            if (!(grade[0] && grade[1] && grade[2]) || !(grade[0] && grade[1] && !grade[2]))
+                iscollect[3] = true;
+        }
+
+    }
     public void Result()
     {  
         //스테이지 여부 따라iscollect 변수값 설정하기//
@@ -50,12 +87,24 @@ public class s_collection : MonoBehaviour {
     public void collection()
     {
         if (collectNum == Collect.one && iscollect[0])
+        {
             image.SetActive(true);
+            Collection.SetActive(false);
+        }
         if (collectNum == Collect.two && iscollect[1])
+        {
             image.SetActive(true);
+            Collection.SetActive(false);
+        }
         if (collectNum == Collect.three && iscollect[2])
+        {
             image.SetActive(true);
+            Collection.SetActive(false);
+        }
         if (collectNum == Collect.four && iscollect[3])
+        {
             image.SetActive(true);
+            Collection.SetActive(false);
+        }
     }
 }
--- 시작화면/Project Poison/Assets/0_main/script/s_main.cs
+++ 시작화면/Project Poison/Assets/0_main/script/s_main.cs
@@ -1,22 +1,42 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class s_main : MonoBehaviour {
 
     public GameObject MainCanvas;
     public GameObject StartCanvas;
     public GameObject CollectionCanvas;
-
+    public GameObject AnimeCanvas;
+    private GameObject Anime;
 
     void Start()
     {
-        MainCanvas.SetActive(true);
+        Anime = GameObject.Find("Anime");
+        //AnimeCanvas.SetActive(true);
+        MainCanvas.SetActive(false);
         StartCanvas.SetActive(false);
         CollectionCanvas.SetActive(false);
+
+        StartCoroutine(StartAnime());
+        
     }
 
     void Update()
     {
+    }
+
+    IEnumerator StartAnime()
+    {
+        for(int i=1; i <= 6; i++)
+        {
+            Debug.Log("들어갔다");
+            Anime.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/anime"+ i);
+            yield return new WaitForSeconds(1f);
+        }
+        MainCanvas.SetActive(true);
+        AnimeCanvas.SetActive(false);
+        yield return null;
     }
 }
